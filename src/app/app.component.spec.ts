baca8b8a124a94d625bc1d80e9de90aac30f5af7
alberto <duque.alberto>
2020-08-30 10:13:45 -0500

    ajuste feature 2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,13 +4,11 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  it('should create the app2', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
@@ -26,6 +24,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('reto-fullstack app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'reto-fullstack app is running!'
+    );
   });
 });
